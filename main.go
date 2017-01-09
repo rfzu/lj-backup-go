208da06ba63fe0451772256fc5a0ccecd68eb31d
rfzu <rall@list.ru>
2017-01-10 02:14:33 +0300

    found better way to save pages

--- main.go
+++ main.go
@@ -8,11 +8,22 @@ import (
 	"net/http"
 )
 
+func Read(url string) {
+	// from https://schier.co/blog/2015/04/26/a-simple-web-scraper-in-go.html
+	resp, _ := http.Get(url)
+	bytes, _ := ioutil.ReadAll(resp.Body)
+
+	fmt.Println("HTML:\n\n", string(bytes))
+	ioutil.WriteFile("test_2.html", bytes, 0644)
+
+	resp.Body.Close()
+}
+
 func Download(url string) {
+	// piece of of shit
 	fmt.Println("donwload", url)
 	resp, _ := http.Get(url)
 	defer resp.Body.Close()
-	fmt.Printf("%T", resp.Body)
 
 	var reader io.ReadCloser
 	switch resp.Header.Get("Content-Encoding") {
@@ -22,12 +33,12 @@ func Download(url string) {
 		reader = resp.Body
 	}
 
-	fmt.Printf("%T", reader)
 	body, _ := ioutil.ReadAll(reader)
 	fmt.Printf("%T", body)
 	ioutil.WriteFile("text.html", body, 0644)
 }
 
 func main() {
-	Download("http://tema.livejournal.com/2408414.html")
+	// Download("http://tema.livejournal.com/2408414.html")
+	Read("http://tema.livejournal.com/2408414.html")
 }
